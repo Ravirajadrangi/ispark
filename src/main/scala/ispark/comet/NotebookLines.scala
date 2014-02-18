75aea94340a08777ecb24f2ca584817f7deca126
Aaron Kimball <aaron@magnify.io>
2014-02-17 20:18:28 -0800

    Use comet to generate/enumerate the codeChunks.
    Creates a notion of a "NotebookSession" that holds all the info for a
    notebook, and a NotebookLines object that renders them to the page.
    
    Notebooks are a collection of "codeChunk" objects; multi-line code fragments.
    
    Add js and css specific to these codeChunks; double clicking a codeChunk
    causes it to turn into an editable text area.

---
+++ src/main/scala/ispark/comet/NotebookLines.scala
@@ -0,0 +1,40 @@
+
+package ispark.comet
+
+import net.liftweb._
+import http._
+import util._
+import Helpers._
+
+/** Holds the lines of code and results to display within one view of a notebook session. */
+class NotebookLines extends CometActor with CometListener {
+
+  /** Each 'codeChunk' represents one box element to add to the page. */
+  private var codeChunks: Vector[String] = Vector()
+
+  def registerWith = NotebookSession
+
+  /**
+   * When receiving a new list of code chunks from the NotebookSession, update our state and
+   * re-render.
+   */
+  override def lowPriority = {
+    case v: Vector[String] => codeChunks = v; reRender()
+  }
+
+  /**
+   * Replace the body of each 'codeChunkText'-classed element within a 'codeChunk'-classed elem
+   * with the associated codeChunk and a prefix consisting of "[n]: ", its index in the vector.
+   *
+   * Set the id of each codeChunkText to codeChunkText_n, and its double-click behavior
+   * is to turn it into a textarea for the user's modification.
+   */
+  def render = {
+    ".codeChunk" #> (codeChunks.zipWithIndex.map({case (chunk, id) => 
+        (".codeChunkText *" #> ("[" + id.toString + "]: " + chunk)
+      & ".codeChunkText [ondblclick]" #> ("enableInnerEdit('codeChunkText_" + id.toString + "')")
+      & ".codeChunkText [id]" #> ("codeChunkText_" + id.toString)
+    )}))
+  }
+
+}
---
+++ src/main/scala/ispark/comet/NotebookSession.scala
@@ -0,0 +1,22 @@
+
+package ispark.comet
+
+import net.liftweb._
+import http._
+import actor._
+
+/**
+ * Holds all state for one session of an open notebook.
+ */
+object NotebookSession extends LiftActor with ListenerManager {
+  /** Each codeChunk represents one input code chunk. */
+  private var codeChunks: Vector[String] = Vector("Type some scala...", "It's good for you.")
+
+  /** An "update" to send to listeners (NotebookLines instances) are the chunks of code. */
+  def createUpdate = codeChunks
+
+  /** New code chunks are appended to the list. */
+  override def lowPriority = {
+    case chunk: String => codeChunks :+= chunk; updateListeners()
+  }
+}
